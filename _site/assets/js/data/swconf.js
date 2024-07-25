4b3d59f443043d0dc121406b3a9b4cf44cd080e2
chenchp_mac <15554505052@163.com>
2024-07-25 23:27:46 +0800

    update files

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1721913719',resources: [
+    cacheName: 'chirpy-1721921141',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
